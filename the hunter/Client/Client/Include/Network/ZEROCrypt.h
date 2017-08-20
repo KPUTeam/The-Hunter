0180358fc06e8e869e1449b813f7b3b1346f3b39
heosungjin <aiuv0324@gmail.com>
2017-08-21 01:30:42 +0900

    기존 프로젝트 제거

--- the hunter/Client/Client/Include/Network/ZEROCrypt.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class CZEROCrypt
-{
-public:
-	static BOOL Encrypt(BYTE* source, BYTE* destination, DWORD length);
-	static BOOL Decrypt(BYTE* source, BYTE* destination, DWORD length);
-};
